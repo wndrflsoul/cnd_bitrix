30ff4f254cedcb17f2d1b75608ae0bbfaf1e386b
Роман <125793593+wndrflsoul@users.noreply.github.com>
2023-05-11 18:59:26 +0700

    Update template.php

--- local/templates/emptysite/components/bitrix/news/template1/bitrix/news.detail/.default/template.php
+++ local/templates/emptysite/components/bitrix/news/template1/bitrix/news.detail/.default/template.php
@@ -1,17 +1,42 @@
-<body>
-<div class="article-card">
-    <div class="article-card__title"><?php $arResult['NAME'] ?></div>
-    <div class="article-card__date"><?php $arResult['DISPLAY_ACTIVE_FROM'] ?></div>
-    <div class="article-card__content">
-        <div class="article-card__image sticky"><img
-                src="<?php $arResult['DETAIL_PICTURE']['SRC'] ?>" data-object-fit="cover"/>
-        </div>
-        <div class="article-card__text">
-            <div class="block-content" data-anim="anim-3"><p><?php $arResult['DETAIL_TEXT'] ?></p>
-            </div>
-            <a class="article-card__button" href="<?php $arResult['LIST_PAGE_URL'] ?>">Назад к новостям</a>
+<?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true)
+    die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+?>
+<div class="news-detail">
+    <div>
+        <div class="article-card__image sticky"><img src="<?= $arResult["DETAIL_PICTURE"]["SRC"] ?>" alt=""
+                data-object-fit="cover" />
         </div>
     </div>
+    <?php if ($arParams["DISPLAY_DATE"] != "N" && $arResult["DISPLAY_ACTIVE_FROM"]): ?>
+        <div "article-card__date">
+            <?= $arResult["DISPLAY_ACTIVE_FROM"] ?>
+        </div>
+    <?php endif; ?>
+
+    <?php if ($arParams["DISPLAY_NAME"] != "N" && $arResult["NAME"]): ?>
+        <div class="article-card__title">
+            <?= $arResult["NAME"] ?>
+        </div>
+
+    <?php endif; ?>
+    <div class="article-card__text">
+        <div class="block-content" data-anim="anim-3">
+            <?= $arResult["DETAIL_TEXT"]; ?>
+        </div>
+
+
+        <a class="article-card__button" href="<?php $arResult['LIST_PAGE_URL'] ?>">Назад к новостям</a>
+    </div>
 </div>
-</body>
-</html>
